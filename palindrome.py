37c1abeceb5c86bbe257f9ae33b497152fefee71
Limary <105609727+limaryg11@users.noreply.github.com>
2024-04-22 11:18:53 -0700

    Create palindrome.py

---
+++ palindrome.py
@@ -0,0 +1,22 @@
+#Time taken to complete = 5 minutes
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        x_string = str(x) #convert int to str
+
+        #instantiate two pointers
+        l = 0 
+        r = len(x_string) - 1
+
+        #while pointers dont overlap
+        while l < r:
+            #check if right and left pointers dont equal eachother
+            if x_string[l] != x_string[r]:
+                return False
+              
+            #otherwise increment left and decrement right
+            l += 1
+            r -= 1
+          
+        #if we exit while loop and it doesn't return False, then it is palindrome
+        return True
